f7f0cf47d250f057aa055107071d0c1a6b4df5ce
relmossaoui <redouan.elmossaoui@cegedim-srh.com>
2021-11-16 21:37:32 +0100

    First commit : add first version tooltip component.

---
+++ src/RedTooltip/src/index.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect, createRef } from "react";
+import './style.css'
+
+import { VALID_DIRECTIONS } from "../../config";
+
+/**
+ * Return a tooltip react component
+ *
+ * @export
+ * @param { Object } {
+ *     dir,
+ *     bgColor,
+ *     color,
+ *     children,
+ *     title
+ * }
+ * @returns { React.Component }
+ */
+export default function ({
+    dir,
+    bgColor,
+    color,
+    children,
+    title
+}) {
+    // create a ref for tooltip title
+
+    let tooltipRef = createRef()
+    // define a reactive class for tooltip title tied to props updates.
+    let [ tooltipClass, setTooltipClass ] = useState('');
+
+    // update tooltip title class 
+    useEffect(() => {
+        // validate passed 'dir' prop and set a default direction of it's not.
+        let tooltipDir = dir in VALID_DIRECTIONS ? dir : VALID_DIRECTIONS.bottom
+        setTooltipClass(`tooltip__title tooltip__title--${tooltipDir}`)
+    }, [ dir ]);
+
+    // update color and background color of tooltip title
+    useEffect(() => {
+        let tooltipStyles = `
+            --tooltip-bg-color  : ${ bgColor || 'grey' };
+            --tooltip-color     : ${ color || 'white' } 
+        `;
+
+        tooltipRef.current.setAttribute('style', tooltipStyles);
+    }, [ bgColor, color ])
+
+    // set visibility && transform styles on mouse over/out to have some animations
+    let setTooltipVisibility = ( isVisible = false ) => {
+        tooltipRef.current.style.visibility =  isVisible ? 'visible' : 'hidden';
+        tooltipRef.current.style.transform =  isVisible ? 'scale(1)' : 'scale(0.9)';
+    }
+
+    return (
+        <div 
+            className="tooltip__target" 
+            onMouseOver={() => setTooltipVisibility(true) }
+            onMouseOut={() => setTooltipVisibility() }
+        > 
+            { children } 
+            <div ref={ tooltipRef } className={ tooltipClass } > { title } </div>
+        </div>
+    )
+}
---
+++ src/config.js
@@ -0,0 +1,6 @@
+export const VALID_DIRECTIONS = {
+    bottom : 'bottom',
+    top : 'top',
+    left : 'left',
+    right : 'right'
+}
